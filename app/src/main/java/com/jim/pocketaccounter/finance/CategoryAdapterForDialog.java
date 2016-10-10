f8b682971608270c3848d6e42e499499aef62adb
root <mailbak36@gmail.com>
2016-10-10 09:14:30 +0500

    main page is ready!!!

--- app/src/main/java/com/jim/pocketaccounter/finance/CategoryAdapterForDialog.java
+++ app/src/main/java/com/jim/pocketaccounter/finance/CategoryAdapterForDialog.java
@@ -2,6 +2,8 @@ package com.jim.pocketaccounter.finance;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,9 @@ import com.jim.pocketaccounter.R;
 import com.jim.pocketaccounter.database.RootCategory;
 import com.jim.pocketaccounter.utils.record.IconWithName;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,10 +47,38 @@ public class CategoryAdapterForDialog extends BaseAdapter {
 	public View getView(final int position, View convertView, ViewGroup parent) {
 		View view = inflater.inflate(R.layout.icon_with_name, parent, false);
 		ImageView ivCategoryListIcon = (ImageView) view.findViewById(R.id.ivIconWithName);
-		int resId = context.getResources().getIdentifier(result.get(position).getIcon(), "drawable", context.getPackageName());
-		ivCategoryListIcon.setImageResource(resId);
+		if (result.get(position).getIcon().startsWith("icon")) {
+			int resId = context.getResources().getIdentifier(result.get(position).getIcon(), "drawable", context.getPackageName());
+			ivCategoryListIcon.setImageResource(resId);
+		}
+		else {
+			Bitmap bitmap = decodeFile(new File(result.get(0).getIcon()));
+			bitmap = Bitmap.createScaledBitmap(bitmap, (int) context.getResources().getDimension(R.dimen.twentyfive_dp),
+					(int) context.getResources().getDimension(R.dimen.twentyfive_dp), false);
+			ivCategoryListIcon.setImageBitmap(bitmap);
+		}
 		TextView tvCategoryListName = (TextView) view.findViewById(R.id.tvIconWithName);
 		tvCategoryListName.setText(result.get(position).getName());
 		return view;
+	}
+	private Bitmap decodeFile(File f) {
+		try {
+//            Decode image size
+			BitmapFactory.Options o = new BitmapFactory.Options();
+//			o.inJustDecodeBounds = true;
+			BitmapFactory.decodeStream(new FileInputStream(f), null, o);
+//            The new size we want to scale to
+			final int REQUIRED_SIZE = 128;
+//            Find the correct scale value. It should be the power of 2.
+			int scale = 1;
+			while (o.outWidth / scale / 2 >= REQUIRED_SIZE && o.outHeight / scale / 2 >= REQUIRED_SIZE)
+				scale *= 2;
+			//Decode with inSampleSize
+			BitmapFactory.Options o2 = new BitmapFactory.Options();
+			o2.inSampleSize = scale;
+			return BitmapFactory.decodeStream(new FileInputStream(f), null, o2);
+		} catch (FileNotFoundException e) {
+		}
+		return null;
 	}
 }
